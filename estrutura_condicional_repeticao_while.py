a1717eb8a7b6a2a4f45fdc44762ea00e4d931269
DevFlorencio <116380905+Florenciobruno@users.noreply.github.com>
2023-07-28 23:16:42 -0300

    Add files via upload

---
+++ estrutura_condicional_repeticao_while.py
@@ -0,0 +1,24 @@
+opcao = -1
+
+while opcao != 0:
+    opcao = int(input("[1] Sacar \n[2] Extrato \[0] Sair \n: "))
+    opcao2 = int(input("[1]Imprimir extrato \n[2]Enviar extrato por email \n[3]Voltar ao menu anterior"))
+    
+    if opcao == 1:
+        print("Sacando...")
+        print("Retire seu dinheio no local indicado")
+    elif opcao == 2:
+          print("Exibindo extrato...")
+          if opcao2 == 1:
+              print("imprimindo extrato")
+elif:
+              opcao2 == 2
+              print("Envio realizado com sucesso")
+            
+elif:
+              opcao2 == 3
+              print("Retornando ao menu anterior...")
+        
+        
+elif:
+            print("Obrigado por utilizar nosso ssitema bancário, até logo")
---
+++ manipulacao_de_strings.py
@@ -0,0 +1,28 @@
+nome = "bruno"
+
+### Comandos para eliminar espaços em branco ###
+
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto + " Olá mundo!                "
+
+print(texto = ".")
+print(texto.strip()) + ".")
+print(texto.rstrip()) + ".")
+print(texto.lstrip()) + ".")
+
+##### Junção e centralização, exemplos.
+
+menu = "Python"
+
+print("####" +Python+"####")
+print(menu.center(14))
+print(menu.center(14, "#"))
+
+
+
+print("-.join")  # separa texto com traço, ex: "P-Y-T-H-O-N"
+
