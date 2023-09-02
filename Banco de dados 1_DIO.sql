7c68059a02dfa520078d7e3bc9aa3ed42e40e2b5
DevFlorencio <116380905+Florenciobruno@users.noreply.github.com>
2023-09-01 23:52:36 -0300

    Desafio DIO Oficina Mecanica
    Descrição do Desafio
    Para este cenário você irá utilizar seu esquema conceitual, criado no desafio do módulo de modelagem de BD com modelo ER, para criar o esquema lógico para o contexto de uma oficina. Neste desafio, você definirá todas as etapas. Desde o esquema até a implementação do banco de dados. Sendo assim, neste projeto você será o protagonista. Tenha os mesmos cuidados, apontados no desafio anterior, ao modelar o esquema utilizando o modelo relacional.
    
    Após a criação do esquema lógico, realize a criação do Script SQL para criação do esquema do banco de dados. Posteriormente, realize a persistência de dados para realização de testes. Especifique ainda queries mais complexas do que apresentadas durante a explicação do desafio. Sendo assim, crie queries SQL com as cláusulas abaixo:
    
    Recuperações simples com SELECT Statement;
    Filtros com WHERE Statement;
    Crie expressões para gerar atributos derivados;
    Defina ordenações dos dados com ORDER BY;
    Condições de filtros aos grupos – HAVING Statement;
    Crie junções entre tabelas para fornecer uma perspectiva mais complexa dos dados;
    Diretrizes
    Não há um mínimo de queries a serem realizadas;
    Os tópicos supracitados devem estar presentes nas queries;
    Elabore perguntas que podem ser respondidas pelas consultas
    As cláusulas podem estar presentes em mais de uma query

--- Banco de dados 1_DIO.sql
+++ Banco de dados 1_DIO.sql
@@ -1,126 +1,120 @@
--- criação de banco de dados para o cenario de e-commerce --
+create database oficina;
 
--- create database ecommerce; 
+CREATE TABLE `veiculo` (
+  `idVeiculo` int NOT NULL AUTO_INCREMENT,
+  `Placa` varchar(45) NOT NULL,
+  `Modelo` varchar(45) NOT NULL,
+  `Marca` varchar(45) NOT NULL,
+  `IdCliente` int NOT NULL,
+  PRIMARY KEY (`idVeiculo`),
+  UNIQUE KEY `Placa_UNIQUE` (`Placa`),
+  KEY `IdCliente_idx` (`IdCliente`),
+  CONSTRAINT `IdCliente` FOREIGN KEY (`IdCliente`) REFERENCES `cliente` (`idCliente`)
+) 
 
-SELECT * FROM cliente;
+INSERT INTO `veiculo` VALUES (1,'QSE-1234','chevrolet','vectra',3);
 
-SELECT * FROM fornecedor;
+DROP TABLE IF EXISTS `cliente`;
 
-SELECT * FROM vendedor;
+CREATE TABLE `cliente` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(45) NOT NULL,
+  `CPF` varchar(45) NOT NULL,
+  PRIMARY KEY (`idCliente`),
+  UNIQUE KEY `CPF_UNIQUE` (`CPF`)
+) 
 
--- Usando a cláusula WHERE--
+INSERT INTO `cliente` VALUES (11,'pedro','19479488846'),(22,'joana','05379217834');
 
-SELECT primeironome, ultimonome, datanascimento, cidade
-FROM cliente
-WHERE sexo = 'M';
+DROP TABLE IF EXISTS `equipe`;
 
-SELECT primeironome, ultimonome, datanascimento, cidade
-FROM cliente
-WHERE sexo = 'M';
+CREATE TABLE `equipe` (
+  `idEquipe` int NOT NULL AUTO_INCREMENT,
+  `Descricao` varchar(45) NOT NULL,
+  PRIMARY KEY (`idEquipe`)
+) 
 
--- Criando expressões para gerar atributos derivados --
+INSERT INTO `equipe` VALUES (1,'TROCA PNEUS'),(2,'TROCA OLEO');
 
-SELECT primeironome, ultimonome, (year(CURDATE()) - year(datanascimento)) as idade
-FROM cliente;
+DROP TABLE IF EXISTS `funcionario`;
 
-SELECT primeironome, ultimonome, (year(CURDATE()) - year(datanascimento)) as idade
-FROM cliente;
-WHERE sexo = 'M';
+CREATE TABLE `funcionario` (
+  `idFuncionario` int NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(45) NOT NULL,
+  `Endereco` varchar(45) NOT NULL,
+  `Especialidade` varchar(45) NOT NULL,
+  `IdEquipe` int NOT NULL,
+  PRIMARY KEY (`idFuncionario`),
+  KEY `IdEquipe_idx` (`IdEquipe`),
+  CONSTRAINT `IdEq` FOREIGN KEY (`IdEquipe`) REFERENCES `equipe` (`idEquipe`)
+) 
 
-SELECT primeironome, ultimonome, (year(CURDATE()) - year(datanascimento)) as idade
-FROM cliente;
-WHERE sex = 'F';
+INSERT INTO `funcionario` VALUES (1,'THAMY','COSMOS','MECANICO',1),(2,'TAY','SANTA CRUZ','MECANICO',2),(3,'LARI','BANGU','MECANICO',1),(4,'MIGUEL','MARGARIDA','MECANICO',2);
 
-SELECT sexo, avg((year(CURDATE()) - year(datanascimento))) as avg_idade
-FROM cliente
-GROUP BY sexo;
+DROP TABLE IF EXISTS `ordem_servico`;
 
-SELECT (year(CURDATE()) - year(datanascimento)) AS idade, count(1)
-FROM cliente
-GROUP BY idade;
+CREATE TABLE `ordemServico` (
+  `idOrdemServico` int NOT NULL AUTO_INCREMENT,
+  `DataEmissao` datetime NOT NULL,
+  `DataConclusao` datetime NOT NULL,
+    `IdCarro` int NOT NULL,
+  `IdEquipe` int DEFAULT NULL,
+  PRIMARY KEY (`idOrdem_Servico`),
+  KEY `IdCarro_idx` (`IdCarro`),
+  KEY `IdEquipe_idx` (`IdEquipe`),
+  CONSTRAINT `IdCarro` FOREIGN KEY (`IdCarro`) REFERENCES `carro` (`idCarro`),
+  CONSTRAINT `IdEquipe` FOREIGN KEY (`IdEquipe`) REFERENCES `equipe` (`idEquipe`)
+) 
 
--- Encomendar com ORDER BY --
 
-SELECT primeironome, ultimonome, (year(CURDATE()) - year(datanascimento)) as idade
-FROM cliente
-ORDER BY firstname;
+INSERT INTO `ordemServico` VALUES (2,'2022-09-23 00:00:00','2022-09-25 00:00:00','Aguardando',1,1),(3,'2022-09-23 00:00:00','2022-09-25 00:00:00','Aguardando',2,2);
 
-SELECT primeironome, ultimonome, (year(CURDATE()) - year(datanascimento)) as idade
-FROM cliente
-ORDER BY idade;
+DROP TABLE IF EXISTS `pecas`;
 
-SELECT (year(CURDATE()) - year(datanascimento)) AS idade, count(1) AS Qtd
-FROM cliente
-GROUP BY idade
-ORDER BY Qtd desc;
+CREATE TABLE `pecas` (
+  `idPecas` int NOT NULL AUTO_INCREMENT,
+  `Descricao` varchar(45) NOT NULL,
+  `Valor` float NOT NULL,
+  PRIMARY KEY (`idPecas`)
+) 
 
-SELECT sexo, count(1) AS Qtd
-FROM cliente
-GROUP BY sexo
-ORDER BY Qtd desc;
+INSERT INTO `pecas` VALUES (1,'Pneu',90),(2,'Oleo',25);
 
--- Agrupando e usando HAVING --
+DROP TABLE IF EXISTS `pecasutilizadas`;
 
-SELECT (year(CURDATE()) - year(datanascimento)) AS idade, count(1) AS Qtd
-FROM cliente
-GROUP BY idade 
-HAVING qtd > 1;
+CREATE TABLE `pecasutilizadas` (
+  `idPecasUtilizadas` int NOT NULL AUTO_INCREMENT,
+  `Qtd` varchar(45) NOT NULL DEFAULT '1',
+  `IdPecas` int DEFAULT NULL,
+  `IdOS` int DEFAULT NULL,
+  PRIMARY KEY (`idPecasUtilizadas`),
+  KEY `IdPecas_idx` (`IdPecas`),
+  KEY `IdOS_idx` (`IdOS`),
+  CONSTRAINT `IdOS` FOREIGN KEY (`IdOS`) REFERENCES `ordem_servico` (`idOrdem_Servico`),
+  CONSTRAINT `IdPecas` FOREIGN KEY (`IdPecas`) REFERENCES `pecas` (`idPecas`)
+) 
 
------------------
--- Usando JOIN
------------------
+INSERT INTO `pecasutilizadas` VALUES (3,'4',1,2),(4,'2',2,3);
 
--- Seleção de preços e de fornecedores --
+DROP TABLE IF EXISTS `servicorealizado`;
 
-SELECT s.registeredname, p.categoria, p.price
-FROM Fornecedor s JOIN ecommerce.`Disponibilidade de um produto` m
-ON 
-	s.idFornecedor = m.idFornecedor
-JOIN produto p
-ON
-	m.idProduto = p.idProduto;
+CREATE TABLE `servicorealizado` (
+  `idServicoRealizado` int NOT NULL AUTO_INCREMENT,
+  `IdServico` int NOT NULL,
+  `IdOS` int NOT NULL,
+  PRIMARY KEY (`idServicoRealizado`),
+  KEY `IdServico_idx` (`IdServico`),
+  KEY `IdOSs_idx` (`IdOS`),
+  CONSTRAINT `IdOSs` FOREIGN KEY (`IdOS`) REFERENCES `servico` (`idServico`),
+  CONSTRAINT `IdServico` FOREIGN KEY (`IdServico`) REFERENCES `servicos` (`idServico`)
+) 
+INSERT INTO `servicorealizado` VALUES (1,1,1),(2,2,2);
 
--- Seleção de fornecedores e preço medio de venda --
-    
-SELECT s.registeredname, p.categoria, ROUND(AVG(p.price),2) as avg_price
-FROM fornecedor s JOIN ecommerce.`making a product available` m
-ON 
-	s.idFornecedor = m.idFornecedor
-JOIN produto p
-ON
-	m.idProduto = p.idProduto
-GROUP BY 
-	s.registeredname;
+DROP TABLE IF EXISTS `servico`;
 
--- Selecionando a compra de cada cliente e a(s) quantidade(s). --
-
-SELECT p.primeironome, p.cidade, pr.categoria, pr.preço, pr.quantidade
-FROM
-	cliente p JOIN ecommerce.order o
-ON
-	p.idCliente = o.idCliente
-JOIN
-	productorder po
-ON
-	o.idOrder = po.idOrder
-JOIN
-	produto pr
-ON
-	po.idProduto = pr.idProduto;
-
--- Quantia que cada cliente gastou --
-
-SELECT p.primeironome, round(sum(pr.preço),2) AS total, sum(pr.quantidade) as total_qtd
-FROM
-	cliente p JOIN ecommerce.order o
-ON
-	p.idCliente = o.idCliente
-JOIN
-	productorder po
-ON
-	o.idOrder = po.idOrder
-JOIN
-	product pr
-ON
-	po.idProduto = pr.idProduto
-GROUP BY p.primeironome;
+CREATE TABLE `servico` (
+  `idServico` int NOT NULL AUTO_INCREMENT,
+  `Descricao` varchar(45) NOT NULL,
+  `Valor` float NOT NULL,
+  PRIMARY KEY (`idServico`)
+) 
